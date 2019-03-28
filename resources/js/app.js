83ee27f08734f6940b2f942bd7ad08d743e9791d
William <william@pixney.com>
2019-03-28 14:13:09 +0100

    ...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+// require('./bootstrap-native-v4');
+var bsn = require('bootstrap.native/dist/bootstrap-native-v4');
 import whenDomReady from 'when-dom-ready';
 import login from "./src/style/login";
 import sideBarToggler from "./src/style/openSidebar";
@@ -7,3 +9,35 @@ whenDomReady().then(() => {
 });
 
 sideBarToggler(".o-navigation__openSidebar");
+
+const myTabs = document.querySelector('.nav-sections');
+var tab = new bsn.Tab(myTabs,{});
+// // first, we reference the .nav component that holds all tabs
+// //var myTabs = document.getElementById('myTabs');
+// var myCarousel = new bsn.Carousel(element,options);
+// const myTabs = document.querySelector('.nav-sections');
+
+// // let's give the initialization a JavaScript reference for the "target" option
+// var myTabsCollection = myTabs.getElementsByTagName('A');
+
+// // initialize the component for all items in the collection
+// for (var i = 0; i < myTabsCollection.length; i++) {
+//   new Tab(myTabsCollection[i], // our target
+//   { // our options
+//     height: true
+//   });
+// }
+
+// // get last item from collection and reference it's initialization
+// var myLastTab = myTabsCollection[myTabsCollection.length-1];
+// var myLastTabInit = myLastTab.Tab;
+
+// // assuming the last tab is not active, we can show it
+// myLastTabInit.show();
+
+// // attach an event handler as well
+// myLastTab.addEventListener('show.bs.tab', function(event){
+//   // do some cool stuff
+//   // event.target is myLastTab
+//   // event.relatedTarget is the previous active tab 
+// }, false);
