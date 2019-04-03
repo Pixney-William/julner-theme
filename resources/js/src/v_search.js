aff534c4a49dcbbac0d22b75f23a9fd257d4a198
William <william@pixney.com>
2019-04-03 23:48:09 +0200

    ...

--- resources/js/src/v_search.js
+++ resources/js/src/v_search.js
@@ -3,10 +3,11 @@
     const _results = d.getElementById('jsSearchResults');
     const _resultsList = _results.querySelector('.m-list');
     const _resultsListItems = _resultsList.querySelectorAll('li:not(.dropdown-header)');
-    const _items = _resultsList.querySelectorAll('li:not(.dropdown-header) a');
+    const _resultsListItemAnchors = _resultsList.querySelectorAll('li:not(.dropdown-header) a');
 
     const _input = d.getElementById('jsSearchInput');
     const _isOnId = (path, id) => path.some(element => element.id === id);
+    let selected = false;
 
     function _setElementSelected(item) {
         item.classList.add('selected')
@@ -19,18 +20,22 @@
     function _selectFirstElementInlist() {
         _setElementSelected(_resultsListItems[0]);
     }
+    function _selectLastElementInlist() {
+        const index = _resultsListItems.length - 1;
+        _setElementSelected(_resultsListItems[index]);
+    }
 
     function _resetList() {
         const selectedListItem = _resultsList.querySelector('li.selected')
         if (selectedListItem) {
             selectedListItem.classList.remove('selected');
         }
+        for (let index = 0; index < _resultsListItemAnchors.length; index++) {
+            _resultsListItemAnchors[index].parentNode.classList.remove('-hide');
+        }
         _input.value = "";
         _input.blur;
         _results.classList.remove('-open');
-        for (let index = 0; index < _items.length; index++) {
-            _items[index].parentNode.classList.remove('-hide');
-        }
 
     }
 
@@ -42,8 +47,16 @@
         }
     }
 
+    /**
+     * When we click the down arrow key, we need to move down the list.
+     * If no element is selected when we click, the first element will
+     * be set as selected.
+     */
     function _down() {
         const selectedListItem = _resultsList.querySelector('li.selected')
+
+        if(!selectedListItem) _selectFirstElementInlist();
+
         const next = selectedListItem.nextElementSibling;
 
         if (next) {
@@ -53,8 +66,19 @@
 
     }
 
+    /**
+     * When we click the up arrow key, we need to move up the list.
+     * If no element is selected when we click, the first element will
+     * be set as selected.
+     */
     function _up() {
         const selectedListItem = _resultsList.querySelector('li.selected')
+        console.log(selectedListItem);
+        if(!selectedListItem) {
+            _selectLastElementInlist();
+            return;
+        }
+        
         const previous = selectedListItem.previousElementSibling;
 
         if (previous) {
@@ -66,19 +90,21 @@
     function _filter() {
 
         const value = _input.value;
-        console.log(_items.length);
-        for (let index = 0; index < _items.length; index++) {
-            const element = _items[index];
+        for (let index = 0; index < _resultsListItemAnchors.length; index++) {
+            const element = _resultsListItemAnchors[index];
 
-            console.log(element.innerHTML.toLowerCase().indexOf(value.toLowerCase()) >= 0);
             if (element.innerHTML.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
                 element.parentNode.classList.remove('-hide');
-
             } else {
                 element.parentNode.classList.add('-hide');
             }
 
         }
+
+    }
+
+    function _followLink() 
+    {
 
     }
 
@@ -97,7 +123,10 @@
         const keyCode = evt.keyCode;
         const jsSearchInputValue = _input.value;
 
-        if (keyCode == 13) return;
+        // if an item is selected, and enter key is clicked. We 
+        // should follow that link..
+        if (keyCode == 13 && !selected) return;
+        if (keyCode == 13 && selected) followLink();
         else if (keyCode == 40) _down();
         else if (keyCode == 38) _up();
         else _filter();
@@ -108,7 +137,7 @@
 
     _input.addEventListener('focus', function (focusEvent) {
         _results.classList.add('-open');
-        _selectFirstElementInlist();
+        //_selectFirstElementInlist();
     })
 
 
